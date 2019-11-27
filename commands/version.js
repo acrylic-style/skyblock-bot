3249c986ae840ef85ea243b2a18ac19ac03509ee
acrylic-style <me@acrylicstyle.xyz>
2019-11-27 09:21:19 +0900

    minor fix

--- commands/version.js
+++ commands/version.js
@@ -9,10 +9,10 @@ module.exports = class extends Command {
 
   async run(msg, lang, args, sendDeletable) {
     const application = await msg.client.fetchApplication()
-    const tag = application.owner.discriminator === '0000' ? '<Owned by Team>' : application.owner.tag
+    const tag = application.owner.discriminator === '0000' ? '<Owned by Team>' : `\`${application.owner.tag}\``
     sendDeletable(`${pkg.name} v${pkg.version} @ ${(await git().revparse(['HEAD'])).slice(0, 7)}
      - Source Code: ${pkg.repository}
-     - Bot owner: \`${tag}\` (ID: ${application.owner.id})
+     - Bot owner: ${tag} (ID: ${application.owner.id})
      - Invite: https://discordapp.com/api/oauth2/authorize?client_id=${msg.client.user.id}&permissions=125952&scope=bot`)
   }
 }
